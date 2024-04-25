3ccc76fe2aaf31e13fb34aa9ac63e19bb9d6dda9
Melki <mortizm28@miumg.edu.gt>
2024-04-24 20:47:41 -0600

    Agregar archivos de proyecto.

---
+++ POO/Clases.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+// Abstracción
+abstract class Personaje
+{
+    public abstract void Atacar();
+    public abstract void Defenderse();
+}
+
+// Herencia
+class NaveEspacial
+{
+    public string Nombre { get; set; }
+    public int Velocidad { get; set; }
+}
+
+class CazaEstelar : NaveEspacial
+{
+    public void DesplegarEscudos()
+    {
+        // No hay Console.WriteLine aquí en la clase CazaEstelar
+    }
+}
+
+// Polimorfismo
+interface IFuerza
+{
+    void UsarFuerza();
+}
+
+class Jedi : Personaje, IFuerza
+{
+    public override void Atacar() { }
+
+    public override void Defenderse() { }
+
+    public void UsarFuerza() { }
+}
+
+class Sith : Personaje, IFuerza
+{
+    public override void Atacar() { }
+
+    public override void Defenderse() { }
+
+    public void UsarFuerza() { }
+}
+
+// Encapsulación
+class Droide
+{
+    private string Tipo { get; set; }
+    public string Funcionalidad { get; set; }
+
+    public Droide(string tipo, string funcionalidad)
+    {
+        Tipo = tipo;
+        Funcionalidad = funcionalidad;
+    }
+}
+
+// Interfaces
+interface IFaccion
+{
+    string ObtenerFaccion();
+}
+
+class Alianza : IFaccion
+{
+    public string ObtenerFaccion()
+    {
+        return "La Alianza Rebelde";
+    }
+}
+
+class Imperio : IFaccion
+{
+    public string ObtenerFaccion()
+    {
+        return "El Imperio Galáctico";
+    }
+}
+
+// Delegados
+delegate void DelegadoLider(Personaje lider);
+
+// Eventos
+class BatallaEventArgs : EventArgs
+{
+    public string Lugar { get; set; }
+    public string Resultado { get; set; }
+}
+
+class Batalla
+{
+    public event EventHandler<BatallaEventArgs> InicioBatalla;
+
+    public void EmpezarBatalla(string lugar)
+    {
+        OnInicioBatalla(new BatallaEventArgs { Lugar = lugar, Resultado = "Indefinido" });
+    }
+
+    protected virtual void OnInicioBatalla(BatallaEventArgs e)
+    {
+        InicioBatalla?.Invoke(this, e);
+    }
+}
+
+// Indexadores
+class Galaxia
+{
+    private string[] planetas = { "Tatooine", "Coruscant", "Endor" };
+
+    public string this[int i]
+    {
+        get { return planetas[i]; }
+        set { planetas[i] = value; }
+    }
+}
+
+// Propiedades
+class Arma
+{
+    private string tipo;
+
+    public string Tipo
+    {
+        get { return tipo; }
+        set { tipo = value; }
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Mensaje de ejemplo
+        Console.WriteLine("Este es el ejemplo de programación orientada a objetos en C#");
+
+        // Ejemplos de uso
+        // Abstracción
+        Console.WriteLine("\nAbstracción:");
+        Jedi obiWanKenobi = new Jedi();
+        Console.WriteLine("El Jedi ataca con su sable de luz.");
+        Console.WriteLine("El Jedi desvía los disparos con la Fuerza.");
+
+        // Herencia
+        Console.WriteLine("\nHerencia:");
+        CazaEstelar xWing = new CazaEstelar();
+        xWing.Nombre = "X-Wing";
+        xWing.Velocidad = 1000;
+        Console.WriteLine($"La nave {xWing.Nombre} tiene una velocidad de {xWing.Velocidad} km/h.");
+        Console.WriteLine("Escudos activados para el caza estelar.");
+
+        // Polimorfismo
+        Console.WriteLine("\nPolimorfismo:");
+        Console.WriteLine("El Jedi usa la Fuerza para mover objetos.");
+        Console.WriteLine("El Sith ejerce su poder para asfixiar a sus enemigos.");
+
+        // Encapsulación
+        Console.WriteLine("\nEncapsulación:");
+        Droide r2d2 = new Droide("Astromecánico", "Reparación");
+        Console.WriteLine($"El droide es de tipo {r2d2.Funcionalidad}.");
+
+        // Interfaces
+        Console.WriteLine("\nInterfaces:");
+        IFaccion rebelde = new Alianza();
+        Console.WriteLine($"La facción es: {rebelde.ObtenerFaccion()}");
+
+        // Delegados
+        Console.WriteLine("\nDelegados:");
+        DelegadoLider liderAlianza = LiderAlianza;
+        liderAlianza(new Jedi());
+
+        // Eventos
+        Console.WriteLine("\nEventos:");
+        Batalla batalla = new Batalla();
+        batalla.InicioBatalla += (sender, e) =>
+        {
+            Console.WriteLine($"La batalla ha comenzado en {e.Lugar}. Resultado: {e.Resultado}");
+        };
+        batalla.EmpezarBatalla("Hoth");
+
+        // Indexadores
+        Console.WriteLine("\nIndexadores:");
+        Galaxia galaxia = new Galaxia();
+        Console.WriteLine($"El tercer planeta de la galaxia es: {galaxia[2]}");
+
+        // Propiedades
+        Console.WriteLine("\nPropiedades:");
+        Arma sableLuz = new Arma();
+        sableLuz.Tipo = "Sable de luz";
+        Console.WriteLine($"El arma del Jedi es un {sableLuz.Tipo}.");
+    }
+
+    static void LiderAlianza(Personaje lider)
+    {
+        Console.WriteLine($"El líder de la Alianza es un {lider.GetType().Name}");
+    }
+}
